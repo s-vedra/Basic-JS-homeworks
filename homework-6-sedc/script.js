542d7560254ed784c3cec019f15abc6c7c61f1ea
Vedra-Stojanovska <vedrastojanovska@gmail.com>
2021-12-31 01:10:44 +0100

    Update script.js

--- homework-6-sedc/script.js
+++ homework-6-sedc/script.js
@@ -1,13 +1,15 @@
+//empty array
 let arrayStory = [];
-function tellStory() {
+//function to get strings in the array and display the message
+function tellStory(array) {
   let position = 1;
   for (i = 0; i < 3; i++) {
-    arrayStory[i] = prompt(`Enter value for position ${position}`);
+    array[i] = prompt(`Enter value for position ${position}`);
     position++;
   }
   console.log(
-    `This is ${arrayStory[0]}. ${arrayStory[0]} is a nice person. Today they are ${arrayStory[1]}. They are ${arrayStory[2]} all day. The end`
+    `This is ${array[0]}. ${array[0]} is a nice person. Today they are ${array[1]}. They are ${array[2]} all day. The end`
   );
 }
-tellStory();
-console.log(arrayStory);
+//call the function
+console.log(tellStory(arrayStory));
