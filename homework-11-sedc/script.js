26983c4abb1ee56e5fb66a9124c38d0a39e84f87
Vedra-Stojanovska <vedrastojanovska@gmail.com>
2021-12-31 01:05:03 +0100

    added comments

--- homework-11-sedc/script.js
+++ homework-11-sedc/script.js
@@ -1,14 +1,15 @@
+//asks how many numbers you want in the empty array
 let numbers = [];
 let n = parseInt(prompt("How many numbers do you want in the array?"));
 if (isNaN(n)) {
   alert("I said numbers");
 }
-
+//pushes the amount of n numbers you typed in the prompt and also add some hardcoded strings
 for (i = 0; i < n; i++) {
   numbers.push(Math.floor(Math.random() * 100));
 }
 numbers.push("random", "strings", "in", "the", "array");
-
+//function to get max num
 function maxNumber(array) {
   let maxNumber = array[0];
   for (i = 0; i < array.length; i++) {
@@ -20,7 +21,7 @@ function maxNumber(array) {
   }
   return maxNumber;
 }
-
+//function to get min num
 function minNumber(array) {
   let minNumber = array[0];
   for (i = 0; i < array.length; i++) {
@@ -32,11 +33,12 @@ function minNumber(array) {
   }
   return minNumber;
 }
+//function to get the sum of the min and max num
 function sumMinMax(array) {
   let sum = maxNumber(array) + minNumber(array);
   return sum;
 }
-
+//call each function
 console.log(numbers);
 console.log(maxNumber(numbers));
 console.log(minNumber(numbers));
