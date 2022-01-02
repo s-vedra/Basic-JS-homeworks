0ec77e4fcbf67ee5d6fb7c4ed17ee721fac9215d
Vedra-Stojanovska <vedrastojanovska@gmail.com>
2022-01-02 18:18:07 +0100

    join multiple strings from an empty array into one big string

---
+++ homework-8-sedc/script.js
@@ -0,0 +1,29 @@
+//empty string
+let stringArray = [];
+
+//n is the number of strings you want to add in the empty array
+let n = parseInt(prompt("How many strings do you want in the array?"));
+let position = 1;
+
+//checking to see if the input is a valid input and push it into the empty array
+if (isNaN(n)) {
+  alert("Wrong input");
+} else {
+  for (i = 0; i < n; i++) {
+    let strings = prompt(`Enter string for position ${position}`);
+    position++;
+    stringArray.push(strings);
+  }
+}
+
+//function to join all the elements into one big string
+function concatString(array) {
+  for (i = 0; i < array.length; i++) {
+    bigString = array.join(" ");
+  }
+  return bigString;
+}
+
+//call the function
+console.log(stringArray);
+console.log(concatString(stringArray));
