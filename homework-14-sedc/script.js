bedc642c6f08b20c2b5814126abc74ba950b502a
Vedra-Stojanovska <vedrastojanovska@gmail.com>
2022-01-10 23:21:46 +0100

    added quantity for the ingredient list and updated style

--- homework-14-sedc/script.js
+++ homework-14-sedc/script.js
@@ -3,45 +3,45 @@ let heading = document.getElementById("heading");
 heading.innerText = prompt("Type the name of your recipe");
 
 //get the element from HTML file
-myDiv = document.getElementById("myDiv");
+let myDiv = document.getElementById("myDiv");
 
 //empty array
-const ingridients = [];
+const ingredients = [];
 
 //function to add elements in the array
-function addIngridients(array) {
-  let n = parseInt(prompt("How many ingridients do you need?"));
+function addIngredients(array) {
+  let n = parseInt(prompt("How many ingredients do you need?"));
   //validate input
   if (isNaN(n)) {
     alert("Wront input");
   } else {
     for (i = 0; i < n; i++) {
-      item = prompt("Enter ingridient");
+      item = prompt("Enter ingredient");
       array.push(item);
     }
   }
   //call the function to print out the items
-  printIngridients(ingridients, myDiv);
+  printIngredients(ingredients, myDiv);
 }
 
 //function to print out the elements
-function printIngridients(array, element) {
-  let list = "<h3>Ingridient list:</h3>";
-  list += `<table>`;
-  let position = 0;
+function printIngredients(array, element) {
+  let list = "<h3>Ingredient list:</h3>";
+  let position = 1;
   for (i = 0; i < array.length; i++) {
+    list += `<div class = "containerTwo">`;
+    list += `<p class="table">${position}. ${array[i]}</p>`;
+    list += `<label for="number" id ="text">Quantity</label>
+    <input type="number" value="1" id=""quantity>`;
+    list += "</div>";
     position++;
-    list += "<tr>";
-    list += `<td class="table">${position}. ${array[i]}</td>`;
-    list += "</tr>";
   }
-  list += "</table>";
   element.innerHTML = list;
 }
 
 //button to print the items
 let button = document
-  .getElementById("printIngridients")
+  .getElementById("printIngredients")
   .addEventListener("click", function () {
-    addIngridients(ingridients);
+    addIngredients(ingredients);
   });
