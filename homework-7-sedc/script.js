1a8ee743cf794f68b2e256417399c87e1b45db55
Vedra-Stojanovska <vedrastojanovska@gmail.com>
2022-01-18 00:28:30 +0100

    Update script.js

--- homework-7-sedc/script.js
+++ homework-7-sedc/script.js
@@ -5,23 +5,25 @@ let numberArray = [];
 function validateNumber(array) {
   for (i = 0; i < 5; i++) {
     let n = parseInt(prompt("Enter number"));
-    if (isNaN(n)) {
-      alert("Wrong input");
-    } else {
+    if (!isNaN(n)) {
       array.push(n);
+    } else {
+      alert("Wrong input");
+      return [];
     }
   }
+  //call the function
+  console.log(`The sum is: ${sumFiveNumbers(numberArray)}`);
 }
 //function to return the sum of the 5 random numbers
 function sumFiveNumbers(array) {
   let sum = 0;
   for (i = 0; i < array.length; i++) {
-    sum = sum + array[i];
+    sum += array[i];
   }
   return sum;
 }
 
 //call the function
 console.log(validateNumber(numberArray));
-console.log(sumFiveNumbers(numberArray));
 console.log(numberArray);
