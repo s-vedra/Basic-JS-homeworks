c93c5a76f37b0c06d83fb9ab70b9c1c0b7d77139
Vedra-Stojanovska <vedrastojanovska@gmail.com>
2022-01-18 17:51:36 +0100

    displaying the reading status of books

--- homework-18-sedc/script.js
+++ homework-18-sedc/script.js
@@ -1,3 +1,4 @@
+//get elements from HTML file
 let title = document.getElementById("title");
 let author = document.getElementById("author");
 let yesButton = document.getElementsByTagName("button")[0];
@@ -5,12 +6,16 @@ let noButton = yesButton.nextElementSibling;
 let read = document.getElementById("readBooks");
 let notRead = document.getElementById("notReadBooks");
 
+//array with objects for read and not read books
 function displayBooks() {
+  //array of objects
   let books = [
+    //read books
     (a = {
       title: "The Robots of dawn",
       author: "Isaac Asimov",
     }),
+    //not read books
     (b = {
       title: "Mockingjay: The Final Book of The Hunger Games",
       author: "Suzanne Collins",
@@ -22,6 +27,7 @@ function displayBooks() {
   console.log(books);
 }
 
+//function to iterate through the array and validate the progress whether the books is read or not
 function validateProgress(array, ul, ulTwo) {
   for (i = 0; i < array.length; i++) {
     if (array[i].readingStatus) {
@@ -42,19 +48,24 @@ function validateProgress(array, ul, ulTwo) {
   }
 }
 
+//call the function
 displayBooks();
 
-//Bonus
+//bonus task
+//empty array
 const library = [];
+//function with an object with the read books
 function readBooks(array) {
   books = {
     title: title.value,
     author: author.value,
   };
+  //validation for the inputs
   if (books.title.length <= 1 || books.author.length <= 1) {
     alert("Wrong input");
   } else {
     array.push(books);
+    //add the reading status
     books.firstAnswer = firstAnswer;
     books.firstAnswer(read);
   }
@@ -62,15 +73,18 @@ function readBooks(array) {
   return [];
 }
 
+//function with an object with the unread books
 function notReadBooks(array) {
   books = {
     title: title.value,
     author: author.value,
   };
+  //validation for the inputs
   if (books.title.length <= 1 || books.author.length <= 1) {
     alert("Wrong input");
   } else {
     array.push(books);
+    //add the reading status
     books.secondAnswer = secondAnswer;
     books.secondAnswer(notRead);
   }
@@ -78,6 +92,7 @@ function notReadBooks(array) {
   return [];
 }
 
+//function to add a property to the object and display the input in an unordered list
 function firstAnswer(ul) {
   this.readingStatus = true;
   let lists = document.createElement("li");
@@ -88,6 +103,7 @@ function firstAnswer(ul) {
   ul.appendChild(lists);
 }
 
+//function to add a property to the object and display the input in an unordered list
 function secondAnswer(ul) {
   this.readingStatus = false;
   let lists = document.createElement("li");
@@ -98,9 +114,11 @@ function secondAnswer(ul) {
   ul.appendChild(lists);
 }
 
+//button to call the function
 yesButton.addEventListener("click", function () {
   readBooks(library);
 });
+//button to call the function
 noButton.addEventListener("click", function () {
   notReadBooks(library);
 });
