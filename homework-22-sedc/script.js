84fff69f418c22ed506799f082ef41cbd479287d
Vedra-Stojanovska <vedrastojanovska@gmail.com>
2022-01-25 16:40:42 +0100

    Update script.js

--- homework-22-sedc/script.js
+++ homework-22-sedc/script.js
@@ -1,8 +1,12 @@
+//get the inputs from HTML
 let toDoInput = document.getElementById("toDoInput");
 let addButton = document.getElementById("addBtn");
 let listUl = document.getElementById("list");
 
+//empty array
 let toDoList = [];
+
+//function to create and push the objects into the array
 let createObject = (name, isCompleted, array) => {
   let list = {
     name: name,
@@ -12,6 +16,7 @@ let createObject = (name, isCompleted, array) => {
   console.log(array);
 };
 
+//function to create the unordered list
 let unorderedList = (element, name) => {
   if (name.length < 2) {
     alert("Wrong input");
@@ -21,22 +26,27 @@ let unorderedList = (element, name) => {
     let span = document.createElement("span");
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.id = "checkBox";
     li.appendChild(checkbox);
-
     span.appendChild(text);
     li.appendChild(span);
+
+    //button to call the function to create the object with its properties
     checkbox.addEventListener("click", function () {
-      createObject(toDoInput.value, true, toDoList);
+      createObject(name, true, toDoList);
     });
+
     element.appendChild(li);
     resetList();
   }
 };
 
+//button to call the function
 addButton.addEventListener("click", function () {
   unorderedList(listUl, toDoInput.value);
 });
 
+//clear the input
 let resetList = () => {
   toDoInput.value = "";
 };
