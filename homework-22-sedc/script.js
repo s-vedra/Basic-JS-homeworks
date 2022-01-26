09bd916067cfaf524fa1b11b6be62959805a8fa2
Vedra-Stojanovska <vedrastojanovska@gmail.com>
2022-01-26 18:13:31 +0100

    update script and style of to do list

--- homework-22-sedc/script.js
+++ homework-22-sedc/script.js
@@ -7,30 +7,32 @@ let listUl = document.getElementById("list");
 let toDoList = [];
 
 //function to create and push the objects into the array
-let createObject = (name, isCompleted, array) => {
-  let list = {
+let createToDo = (name, isCompleted, array) => {
+  let toDoObject = {
     name: name,
     isCompleted: isCompleted,
   };
-  array.push(list);
+  array.push(toDoObject);
   console.log(array);
 };
 
 //function to create the unordered list
-let unorderedList = (element, name) => {
+let createList = (element, name) => {
   if (name.length < 2) {
     alert("Wrong input");
   } else {
     let li = document.createElement("li");
     let text = document.createTextNode(`${name}`);
+    let span = document.createElement("span");
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.id = "checkBox";
-    element.appendChild(checkbox);
-    li.appendChild(text);
+    li.appendChild(checkbox);
+    span.appendChild(text);
+    li.appendChild(span);
     //button to call the function to create the object with its properties
     checkbox.addEventListener("change", function () {
-      createObject(name, true, toDoList);
+      createToDo(name, true, toDoList);
     });
 
     element.appendChild(li);
@@ -40,7 +42,7 @@ let unorderedList = (element, name) => {
 
 //button to call the function
 addButton.addEventListener("click", function () {
-  unorderedList(listUl, toDoInput.value);
+  createList(listUl, toDoInput.value);
 });
 
 //clear the input
